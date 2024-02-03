05b59b70c2bef15d3dd75273ef6750a5a5c5408d
LmOpe <lawalmuhammedope@gmail.com>
2024-02-03 23:35:21 +0100

    Configure tailwind to use the given style guide

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="text-3xl font-bold underline text-center">Health and Fitness!</h1>
+      <h1 className="font-montserrat text-3xl text-primary-6 font-bold underline text-center">Health and Fitness!</h1>
     </div>
   );
 }
