c9009a825b49b5aac3d05ee78cd6c8d98a2afa22
Megha N <n.megha82@gmail.com>
2024-04-08 15:00:56 -0700

    fix:Profile Screen next sections changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import InitialScreen from "./pages/onBoarding/InitialScreen";
 import SignUp from "./pages/signup/SignUp";
 import OnBoarding from "./pages/onBoarding/OnBoarding";
 import Login from "./pages/login/Login";
-import ProfileScreen from "./pages/profile/ProfileScreen";
 import ProfileScreenMain from "./pages/profile/ProfileScreenMain";
 
 function App() {
@@ -14,7 +13,6 @@ function App() {
         <Route path="/on-boarding" element={<OnBoarding />} />
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/log-in" element={<Login />} />
-        <Route path="/profile" element={<ProfileScreen />} />
         <Route path="/profile" element={<ProfileScreenMain />} />
       </Routes>
     </Router>
