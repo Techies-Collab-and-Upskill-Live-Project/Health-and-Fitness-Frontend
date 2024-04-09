117102422411f34ef95cda68084943b0b929dd0d
Megha N <n.megha82@gmail.com>
2024-04-08 17:47:07 -0700

    fix:Profile Screen next sections changes

--- src/pages/profile/ProfileScreenMain.jsx
+++ src/pages/profile/ProfileScreenMain.jsx
@@ -2,27 +2,33 @@ import { useEffect, useState } from "react";
 import ProfileGenderScreen from "./ProfileGenderScreen";
 import ProfileNutritionalGoal from "./ProfileNutritionalGoal";
 import ProfileDOBScreen from "./ProfileDOBScreen";
+import ProfileWeightScreen from "./ProfileWeightScreen";
 
 export default function ProfileScreen() {
   const [isLoading, setIsLoading] = useState(true);
   const [showGenderScreen, setShowGenderScreen] = useState(false);
   const [showDOBScreen, setshowDOBScreen] = useState(false);
-
+  const [showWeightScreen,setshowWeightScreen]=useState(false);
   return (
     <div className="grid justify-center items-center relative my-0 mx-auto w-screen h-screen max-w-screen-sm">
-      {isLoading && !showGenderScreen && !showDOBScreen && (
+      {isLoading && !showGenderScreen && !showDOBScreen && !showWeightScreen && (
         <div className="absolute z-[2]">
           <ProfileNutritionalGoal setShowGenderScreen={setShowGenderScreen} />
         </div>
       )}
-      {showGenderScreen && !showDOBScreen && (
+      {showGenderScreen && !showDOBScreen && !showWeightScreen && (
         <div className="absolute z-[2]">
           <ProfileGenderScreen setshowDOBScreen={setshowDOBScreen}/>
         </div>
       )}
-       {showDOBScreen && (
+       {showDOBScreen && !showWeightScreen &&(
         <div className="absolute z-[2]">
-          <ProfileDOBScreen/>
+          <ProfileDOBScreen setshowWeightScreen={setshowWeightScreen}/>
+        </div>
+      )}
+       {showWeightScreen &&(
+        <div className="absolute z-[2]">
+          <ProfileWeightScreen/>
         </div>
       )}
     </div>
