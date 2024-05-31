07748ea24c1afc658539442b73b6133f17830271
LmOpe <lawalmuhammedope@gmail.com>
2024-05-31 04:02:37 -0700

    Rename Add Meal folder -> AddMeal

---
+++ src/pages/diary/sections/AddMeal/SaveBtn.jsx
@@ -0,0 +1,9 @@
+import { Button } from "../../../../components/Button";
+
+export function SaveBtn() {
+  return (
+    <Button width="w-full" bgColor="bg-primary-9" type="button" mt="mt-10">
+      Save meal
+    </Button>
+  );
+}
