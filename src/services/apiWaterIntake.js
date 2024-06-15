5526fa0f0238e3f0ef2bd9b25c3141e0a2929e14
LmOpe <lawalmuhammedope@gmail.com>
2024-06-16 00:43:59 +0100

    Update the water intake section dynamically with the water intake data

---
+++ src/services/apiWaterIntake.js
@@ -0,0 +1,22 @@
+import { decrypt } from "../utils/helpers";
+import { BASE_URL } from "./apiAuths";
+
+export async function getUserWaterIntake(date) {
+  const token = decrypt(localStorage.getItem("access"));
+  try {
+    const response = await fetch(
+      `${BASE_URL}/api/v1/food-diaries/water-intake/?date=${date}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    const resData = await response.json();
+    return { data: resData, status: response.status };
+  } catch (error) {
+    console.error("Error fetching user waterintake", error);
+  }
+}
